eb66433a1456b815eeca93f53a2cf56be0466fbf
Roland Sebestyen <sebestyenroland21@gmail.com>
2015-11-08 18:34:41 +0200

    Implemented universal notifications

--- notification.rb
+++ notification.rb
@@ -1,12 +1,29 @@
-require 'libnotify'
+require './os'
+
+if Os.linux?
+  require 'libnotify'
+else 
+  require 'terminal-notifier'
+end
 
 class Notification
 
   def self.show_notification(title, message)
-    Libnotify.show(
-        :body    => message,
-        :summary => title,
-        :timeout => 1.5
-    )
+    if Os.linux?
+      Libnotify.show(
+          :body    => message,
+          :summary => title,
+          :timeout => 1.5
+      )
+    elsif Os.mac?
+      TerminalNotifier.notify(
+        message,
+        :title   => title
+      )
+    else
+      raise NotImplemented
+    end
   end
 end
+
+Notification.show_notification('dsdsds', 'dsdsds')
